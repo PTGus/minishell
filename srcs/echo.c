4c00fa72377c9c18180279deb9891dcf30acb5f8
Gustavo Mendes Da Cunha <gumendes@c1r5s5.42porto.com>
2025-04-04 16:33:13 +0100

    Added the built-in echo funtion.

---
+++ srcs/echo.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gumendes <gumendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 11:09:31 by gumendes          #+#    #+#             */
+/*   Updated: 2025/04/04 15:48:23 by gumendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	ft_echo(char **split)
+{
+	int	i;
+
+	if (ft_strncmp(split[1], "-n", 2) == 0)
+	{
+		i = 2;
+		while (split[i])
+		{
+			printf("%s", split[i]);
+			if (split[i + 1] != NULL)
+				printf(" ");
+			i++;
+		}
+	}
+	else
+	{
+		i = 1;
+		while (split[i])
+		{
+			printf("%s", split[i]);
+			if (split[i + 1] != NULL)
+				printf(" ");
+			i++;
+		}
+		printf("\n");
+	}
+}
