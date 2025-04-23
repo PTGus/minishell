156d18ac4fbb7fc39dfbfd8486c6527031d865c8
Gustavo Mendes Da Cunha <gumendes@c1r5s5.42porto.com>
2025-04-23 17:21:55 +0100

    Changes in order to improve readability

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gumendes <gumendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 15:42:20 by gumendes          #+#    #+#             */
-/*   Updated: 2025/04/22 14:54:55 by gumendes         ###   ########.fr       */
+/*   Updated: 2025/04/23 17:09:57 by gumendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ typedef struct s_envp
 	struct s_envp	*prev;
 	char			*var;
 	char			*value;
+	int				index;
 }	t_envp;
 
 //--------------------------------------------------------------//
@@ -42,17 +43,6 @@ typedef struct s_envp
 int		main(int ac, char **av, char **env);
 void	do_cmd(char **split, t_envp **dupenv);
 void	handle_signals(void);
-
-//--------------------------------------------------------------//
-
-// UTILS //
-int		ft_strcmp(char *s1, char *s2);
-void	ft_free_split(char **str);
-char	*pather(char *cmd);
-
-// lst_utils //
-void	ft_lst_back(t_envp **dupenv, t_envp *curr);
-t_envp	*lstlast(t_envp *dupenv);
 
 //--------------------------------------------------------------//
 
@@ -73,12 +63,41 @@ t_envp	*new_env(char *envp);
 int		init_env(t_envp **dupenv, char **envp);
 int 	duplicate_env(t_envp **dupenv, char **envp);
 
+// exit //
+void	ft_exit(t_envp **dupenv);
+
 // export //
 void	ft_export(char **split, t_envp **dupenv);
+int		should_revalue(t_envp **dupenv, char**split);
+void	insert_before_last(t_envp **dupenv, t_envp *curr);
 
 // pwd //
 void	ft_pwd(void);
 
 //--------------------------------------------------------------//
+
+// CLEAN_UP //
+
+// free //
+void	ft_envdelone(t_envp *dupenv);
+void	free_env(t_envp **dupenv);
+
+// UTILS //
+
+// env_utils //
+void	organise_env(t_envp **dupenv);
+void	reorder_dupenv(t_envp **dupenv);
+
+// lst_utils //
+void	ft_lst_back(t_envp **dupenv, t_envp *curr);
+t_envp	*lstlast(t_envp *dupenv);
+
+// utils //
+int		ft_strcmp(char *s1, char *s2);
+void	ft_free_split(char **str);
+char	*pather(char *cmd);
+
+//--------------------------------------------------------------//
+
 
 #endif
