614731e678446c00448e82eee9039389a6ed9bdb
David Nuno Fernandes Roque <david-fe@c1r6s6.42porto.com>
2025-05-28 16:02:00 +0100

    have conditional jump issues to resolve

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -11,8 +11,68 @@
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+/*
+int ft_get_expand_end(char *str, int i, int quote_type)
+{
+	int j;
+
+	j = 0;
+	while (str[i + j])
+	{
+		if (quote_type == UNQUOTED)
+		{
+
+		}
+		else if (quote_type == DOUBLE_Q)
+		{
+
+		}
+		j++;
+	}
+}
+*/
+//iter through list
+//if a $ is found unquoted use getenv to substitute
+//remeber double quotes vs single
+void ft_check_expand(t_input *node)
+{
+	int i;
+
+	i = -1;
+	while (node->value[++i])
+	{
+		if (node->value[i] == '$' && node->value[i + 1]
+			&& ft_is_quoted(node->value, i) != 1)
+		{
+			printf("expander at %i!\n", i);
+			/*
+			if (ft_is_quoted(node->value[i + 1]) == UNQUOTED) 
+				ft_get_expand_end(node->value, i + 1, 0);
+			else if (ft_is_quoted(node->value[i + 1]) == DOUBLE_Q)
+				ft_get_expand_end(node->value, i + 1, 2);
+			*/
+		}
+	}
+}
 
 int	ft_expander(t_central *central)
 {
+	int		i;
+	t_input *current;
+	t_input *next;
+
+	i = -1;
+	while (central->cmd[++i])
+	{
+		current = central->cmd[i];
+		while (current)
+		{
+			next = current->next;
+			if (next->token == 0)
+				ft_check_expand(next);
+			current = next;
+		}
+
+	}
 	return (0);
 }
