3652a83dadd31a2fadb46db9b5868a65a37a9d61
David Nuno Fernandes Roque <david-fe@c1r6s6.42porto.com>
2025-05-30 11:44:05 +0100

    fixed space skip

---
+++ srcs/parsing/print_utils.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david-fe <david-fe@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 10:21:14 by david-fe          #+#    #+#             */
+/*   Updated: 2025/05/30 10:21:39 by david-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/**
+ * @brief Prints an array of strings for debugging
+ * @param str_arr Array of strings to print
+ */
+void	ft_print_arr(char **str_arr)
+{
+	int	i;
+
+	i = 0;
+	while (str_arr && str_arr[i])
+	{
+		printf("%d: '%s'\n", i, str_arr[i]);
+		i++;
+	}
+}
+
+void	ft_print_list_array(t_input **array)
+{
+	int		i;
+	t_input	*temp;
+
+	i = -1;
+	while (array[++i])
+	{
+		temp = array[i];
+		while (temp)
+		{
+			printf("l%i:'%s'(t:%i)\n", temp->index, temp->value, temp->token);
+			temp = temp->next;
+		}
+		printf("\n");
+	}
+}
+
