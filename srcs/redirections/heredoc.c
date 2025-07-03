d971bdb83330a6b7fc5cd7a454b59cac839774b8
Gustavo Mendes Da Cunha <gumendes@c1r5s5.42porto.com>
2025-07-03 15:46:15 +0100

    The saga of making heredoc work continues, yet it resists

--- srcs/redirections/heredoc.c
+++ srcs/redirections/heredoc.c
@@ -6,37 +6,46 @@
 /*   By: gumendes <gumendes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:56:03 by gumendes          #+#    #+#             */
-/*   Updated: 2025/07/02 13:43:45 by gumendes         ###   ########.fr       */
+/*   Updated: 2025/07/03 11:27:12 by gumendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// remember to check if prompt is quoted when you have the full project so you can call the expander accordingly
+static char	*treat_rl_doc(t_central *central ,char *rl_doc, int doc_type)
+{
+	if (doc_type == HERE_DOC)
+		return (ft_check_expand_doc(central, rl_doc));
+	else
+		return (ft_strdup(rl_doc));
+}
+
 int	ft_heredoc(t_central *central, char *delimiter, int doc_type)
 {
 	int		fd;
+	char	*tmp;
 	char	*rl_doc;
 
 	if (check_for_bad_redir(delimiter) == 1)
 		return (2);
 	setup_heredoc_signals();
 	fd = open(".heredoc_tmp", O_CREAT | O_TRUNC | O_RDWR, 0644);
+	tmp = NULL;
 	while (1)
 	{
 		rl_doc = readline("> ");
 		if (!rl_doc || ft_strcmp(rl_doc, delimiter) == 0)
 			break ;
 		if (g_signal == 130)
-			return (130);
-		if (doc_type == HERE_DOC)
-			ft_str_expand(central, rl_doc);
-		ft_putendl_fd(rl_doc, fd);
-		free(rl_doc);
+			return (clean_doc(rl_doc, tmp), 130);
+		tmp = treat_rl_doc(central, rl_doc, doc_type);
+		ft_putendl_fd(tmp, fd);
+		clean_doc(rl_doc, tmp);
+		tmp = NULL;
 	}
-	clean_doc(rl_doc);
+	clean_doc(rl_doc, tmp);
 	if (!rl_doc)
-		return (unlink(".heredoc_tmp"), close(fd), bad_doc(delimiter), 0);
+		return (redirect_to_doc(fd), bad_doc(delimiter), 0);
 	return (redirect_to_doc(fd), 0);
 }
 
