04d89a9b31c9dec7dde81979daa0db80e00c9ca4
Gustavo Mendes Da Cunha <gumendes@c1r5s5.42porto.com>
2025-07-10 17:34:39 +0100

    Integrated support to the new way heredoc is handled

--- srcs/pipes/pipe.c
+++ srcs/pipes/pipe.c
@@ -6,15 +6,17 @@
 /*   By: gumendes <gumendes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:01:44 by gumendes          #+#    #+#             */
-/*   Updated: 2025/07/08 13:29:58 by gumendes         ###   ########.fr       */
+/*   Updated: 2025/07/10 16:55:14 by gumendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	execute_pipes(t_central *central, t_input *cmd, \
+void	execute_pipes(t_central *central, t_input *cmd,
 			int (*pipe_fd)[2], int curr_index)
 {
+	central->curr_cmd_idx = curr_index;
+	central->curr_heredoc_idx = 0;
 	set_pipe_fds(pipe_fd, central->matrix_len - 1, curr_index);
 	close_unused_pipes(pipe_fd, central->matrix_len - 1, curr_index);
 	if (has_to_redirect(central, cmd) != 0)
@@ -49,6 +51,5 @@ void	piper(t_central *central)
 	while (++i < central->matrix_len)
 		waitpid(pid[i], &status, 0);
 	central->exit_val = (status >> 8) & 0xFF;
-	free(pid);
-	free(pipe_fd);
+	return (free(pid), free(pipe_fd), (void) 0);
 }
